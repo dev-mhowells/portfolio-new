4a0af74291397a458ce404a372167d4ab8ba0b4f
Mike <dev.mhowells@gmail.com>
2022-06-13 15:41:39 +0100

    added rising circle animation and contact section

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const homeSection = document.querySelector(".home-section");
 const projectsSection = document.querySelector(".projects-section");
 const aboutSection = document.querySelector(".about-section");
-
+const contactSection = document.querySelector(".contact-section");
 const smallCircles = document.querySelectorAll(".small-circle");
+const risingCircle = document.querySelector(".rising-circle");
 
 const mainChildren = document.getElementById("main").children;
 
@@ -19,18 +20,33 @@ const options = {
   threshold: 0.5,
 };
 
-const homeObserver = new IntersectionObserver(function (entries, observer) {
+const sectionObserver = new IntersectionObserver(function (entries, observer) {
   entries.forEach((entry, index) => {
-    // finds position of target section in array of sections in main
+    // finds position of target section in array of sections in <main>
     const targetIndex = Array.from(mainChildren).indexOf(entry.target);
-    // cirlce is added to corresponding nav item as nav and mian have same order
+    // circle is added to corresponding nav item as nav and main have same order
     if (entry.isIntersecting) {
+      console.log(risingCircle.classList);
       removeCircles();
       smallCircles[targetIndex].classList.add("show-display");
+
+      if (targetIndex === 2) {
+        risingCircle.classList.remove("fall");
+        risingCircle.classList.add("rise");
+      }
+      if (targetIndex === 1 && risingCircle.classList.contains("rise")) {
+        risingCircle.classList.remove("rise");
+        risingCircle.classList.add("fall");
+      }
+      if (targetIndex === 3) {
+        // risingCircle.classList.remove("rise");
+        // risingCircle.classList.add("center");
+      }
     }
   });
 }, options);
 
-homeObserver.observe(homeSection);
-homeObserver.observe(projectsSection);
-homeObserver.observe(aboutSection);
+sectionObserver.observe(homeSection);
+sectionObserver.observe(projectsSection);
+sectionObserver.observe(aboutSection);
+sectionObserver.observe(contactSection);
